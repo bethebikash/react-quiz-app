b92d3ad19218cbc8c4d89f713b69ab5132fdd6b6
bethebikash <bhattaraibikash55@gmail.com>
2021-01-19 11:14:54 +0545

    Quiz component UI design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css'
 import Login from './Login'
+import Quiz from './Quiz'
+import Register from './Register'
 
 function App() {
     return (
         <div className="App">
             <h1>Quiz App</h1>
-            <Login />
+            <Quiz />
         </div>
     )
 }
---
+++ src/Quiz.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react'
+
+import './Quiz.css'
+
+export default class Quiz extends Component {
+    render() {
+        return (
+            <div className="quiz-container">
+                <div className="quiz">
+                    <div className="question-card">
+                        <div className="question-count">
+                            <span>Question 1</span>/ 10
+                        </div>
+                        <div className="question-text">What is what?</div>
+                    </div>
+                    <div className="answer-card">
+                        <form className="answer-form">
+                            <div className="options">
+                                <div>
+                                    <input
+                                        type="radio"
+                                        name="ans"
+                                        id="Answer1"
+                                        value="Answer 1"
+                                    />
+                                    <label htmlFor="Answer1">Answer 1</label>
+                                </div>
+                                <div>
+                                    <input
+                                        type="radio"
+                                        name="ans"
+                                        id="Answer2"
+                                        value="Answer 2"
+                                    />
+                                    <label htmlFor="Answer2">Answre 2</label>
+                                </div>
+                                <div>
+                                    <input
+                                        type="radio"
+                                        name="ans"
+                                        id="Answer3"
+                                        value="Answer 3"
+                                    />
+                                    <label htmlFor="Answer3">Answer 3</label>
+                                </div>
+                                <div>
+                                    <input
+                                        type="radio"
+                                        name="ans"
+                                        id="Answer4"
+                                        value="Answer 4"
+                                    />
+                                    <label htmlFor="Answer4">Answer 4</label>
+                                </div>
+                                <button type="submit" className="answer-submit">Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
