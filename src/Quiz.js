7fa182aea0547b2e399e2e9e6cc25940e1ecd028
bethebikash <bhattaraibikash55@gmail.com>
2021-01-19 14:49:28 +0545

    Displaying the correct/incorrect messaga and changing the submit button to next button.

--- src/Quiz.js
+++ src/Quiz.js
@@ -14,9 +14,13 @@ export default class Quiz extends Component {
             score: 0,
             showScore: false,
             selectError: false,
+            next: false,
+            message: '',
         }
         this.handleAnswerSubmit = this.handleAnswerSubmit.bind(this)
         this.onValueChange = this.onValueChange.bind(this)
+        this.handleRestart = this.handleRestart.bind(this)
+        this.handleNext = this.handleNext.bind(this)
     }
 
     getQuestions = () => {
@@ -46,24 +50,46 @@ export default class Quiz extends Component {
             ) {
                 this.setState({
                     score: this.state.score + 1,
-                })
-            }
-
-            const nextQuestion = this.state.currnetQuestion + 1
-            if (nextQuestion < this.state.qSet.length) {
-                this.setState({
-                    currnetQuestion: nextQuestion,
+                    message: 'Correct Answer',
+                    next: true,
                 })
             } else {
                 this.setState({
-                    showScore: true,
+                    message: 'Wrong Answer',
+                    next: true,
                 })
             }
         }
     }
 
+    handleNext() {
+        const nextQuestion = this.state.currnetQuestion + 1
+        if (nextQuestion < this.state.qSet.length) {
+            this.setState({
+                currnetQuestion: nextQuestion,
+                next: false,
+                selectedOption: '',
+            })
+        } else {
+            this.setState({
+                showScore: true,
+            })
+        }
+    }
+
     componentDidMount() {
         this.getQuestions()
+    }
+
+    handleRestart() {
+        this.getQuestions()
+        this.setState({
+            currnetQuestion: 0,
+            selectedOption: '',
+            score: 0,
+            showScore: false,
+            selectError: false,
+        })
     }
 
     render() {
@@ -75,7 +101,12 @@ export default class Quiz extends Component {
                             <div class="score-text">
                                 You have scored {this.state.score} out of 10.
                             </div>
-                            <div className="reset-button">Reset</div>
+                            <div
+                                className="reset-button"
+                                onClick={this.handleRestart}
+                            >
+                                Reset
+                            </div>
                         </div>
                     ) : (
                         <>
@@ -94,54 +125,80 @@ export default class Quiz extends Component {
                                         </div>
                                     </div>
                                     <div className="answer-card">
-                                        <form
-                                            className="answer-form"
-                                            onSubmit={this.handleAnswerSubmit}
-                                        >
-                                            <div className="options">
-                                                {this.state.qSet[
-                                                    this.state.currnetQuestion
-                                                ].answers.map(
-                                                    (answerOption) => (
-                                                        <div key={answerOption}>
-                                                            <input
-                                                                type="radio"
-                                                                name="ans"
-                                                                id={
-                                                                    answerOption
-                                                                }
-                                                                value={
-                                                                    answerOption
-                                                                }
-                                                                checked={
-                                                                    this.state
-                                                                        .selectedOption ===
-                                                                    answerOption
-                                                                }
-                                                                onChange={
-                                                                    this
-                                                                        .onValueChange
-                                                                }
-                                                            />
-                                                            <label
-                                                                htmlFor={
+                                        {this.state.next ? (
+                                            <>
+                                                <div>{this.state.message}</div>
+                                                <button
+                                                    onClick={this.handleNext}
+                                                    className="answer-submit"
+                                                >
+                                                    Next
+                                                </button>
+                                            </>
+                                        ) : (
+                                            <form
+                                                className="answer-form"
+                                                onSubmit={
+                                                    this.handleAnswerSubmit
+                                                }
+                                            >
+                                                <div className="options">
+                                                    {this.state.qSet[
+                                                        this.state
+                                                            .currnetQuestion
+                                                    ].answers.map(
+                                                        (answerOption) => (
+                                                            <div
+                                                                key={
                                                                     answerOption
                                                                 }
                                                             >
-                                                                {answerOption}
-                                                            </label>
-                                                        </div>
-                                                    )
-                                                )}
-                                                <button
-                                                    type="submit"
-                                                    className="answer-submit"
-                                                >
-                                                    Submit
-                                                </button>
-                                            </div>
-                                            <span>slecct</span>
-                                        </form>
+                                                                <input
+                                                                    type="radio"
+                                                                    name="ans"
+                                                                    id={
+                                                                        answerOption
+                                                                    }
+                                                                    value={
+                                                                        answerOption
+                                                                    }
+                                                                    checked={
+                                                                        this
+                                                                            .state
+                                                                            .selectedOption ===
+                                                                        answerOption
+                                                                    }
+                                                                    onChange={
+                                                                        this
+                                                                            .onValueChange
+                                                                    }
+                                                                />
+                                                                <label
+                                                                    htmlFor={
+                                                                        answerOption
+                                                                    }
+                                                                >
+                                                                    {
+                                                                        answerOption
+                                                                    }
+                                                                </label>
+                                                            </div>
+                                                        )
+                                                    )}
+                                                    <button
+                                                        type="submit"
+                                                        className="answer-submit"
+                                                    >
+                                                        Submit
+                                                    </button>
+                                                    {this.state.selectError && (
+                                                        <span className="selection-error">
+                                                            slecct
+                                                        </span>
+                                                    )}
+                                                </div>
+                                            </form>
+                                        )}
                                     </div>
                                 </>
                             )}
